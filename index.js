b385de3740ed1362473f509ace3a73d43cc5edd9
cool-Blue <cool.blue@y7mail.com>
2016-07-25 02:29:52 +1000

    index.js -- added feature to accept a stream or a file path -- made variable names more general

--- index.js
+++ index.js
@@ -7,30 +7,33 @@ module.exports = listenAll;
 var fs = require('fs');
 var util = require('util');
 var now = require('moment');
+var stream = require('stream');
 
-function listenAll (logFile) {
+function listenAll (output) {
     var _pad        = require('left-pad'),
         _w = 0, _w2 = 0;
     var fd, _logStream;
 
-    if(logFile) {
-        _logStream = fs.createWriteStream(logFile);
+    if(output) {
+        _logStream = (output instanceof require("events") && typeof output.write === 'function')
+            ? output
+            : fs.createWriteStream(output);
         _logStream.on('finish', function() {
             console.log('file has been written');
         });
     }
     var _log = (() => _logStream ? m => _logStream.write(m + '\n') : console.log.bind(console))();
 
-    function _listenAll(stream, events, excl) {
-        _w = Math.max(_w, stream.name ? stream.name.length : 0);
+    function _listenAll(host, events, excl) {
+        _w = Math.max(_w, host.name ? host.name.length : 0);
         var _excl = excl ? Array.isArray(excl) ? excl : [excl] : excl;
         events
             .forEach(function(e) {
                 _w2 = Math.max(e.length, _w2);
                 if(!_excl || !_excl.find(x => x === e))
-                    stream.on(e, function(evt) {
+                    host.on(e, function(evt) {
                         var stamp = now(Date.now()).format("HH:mm:s:SSSS");
-                        _log(`${stamp}\t${_pad(stream.name, _w)} --> ${e}`)
+                        _log(`${stamp}\t${_pad(host.name, _w)} --> ${e}`)
                     })
             })
     }
